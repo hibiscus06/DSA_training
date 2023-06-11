defc821af3c34c87b353cf6bc6cd706cdf1170fe
Rudrika Singh <72783505+hibiscus06@users.noreply.github.com>
2023-06-11 14:46:16 +0530

    Add files via upload
    Added a file

--- Recursion1.java
+++ Recursion1.java
@@ -1,5 +1,5 @@
 import java.net.SocketPermission;
-
+import java.util.*;
 import javax.sound.sampled.SourceDataLine;
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 
@@ -45,7 +45,6 @@ class Solution{
         return sum;
 
     }
-
 
 //series 1/1^1 + 2 / 2 ^2 + 3 / 3^3 
 static double series(int num,double sum)
@@ -106,6 +105,56 @@ public static int getDigitCount(int n) {
     return n == 0 ? 0 : 1 + getDigitCount(n / 10);
 }
 
+//last index of
+public static void lastIndexOf(int[] arr,int el,int i, ArrayList<Integer> cpy,int k)
+{
+    if(i==arr.length)
+    {
+        return;
+    }
+    if(arr[i]==el)
+    {
+        cpy.add(i);
+    }
+     lastIndexOf(arr, el, i+1, cpy, k+1);
+}
+
+//replace value
+public static void replace(int[] arr,int el,int r,int i)
+{
+    if(i==arr.length)
+    {
+        for(int j=0;j<arr.length;j++)
+        {
+            System.out.print(arr[j]+" ");
+        }
+        return ;
+    }
+
+    if(arr[i]==el)
+    {
+        arr[i]=r;
+    }
+
+    replace(arr,el,r,i+1);
+}
+
+//Given a String find the adjacent element if it is same as the previous element so replace the element with #.
+
+public static String repadj(String str,int i,StringBuilder sb)
+{
+    if(i==sb.length())
+    {
+        //System.out.print(str);
+        return sb.toString();
+    }
+    if(sb.charAt(i)==sb.charAt(i-1))
+    {
+        sb.replace(i,i+1,"#");
+    }
+
+    return repadj(str,i+1,sb);
+}
 public static void main(String[] args){
     //System.out.println(pow(12,2));
     //count0(102030,0);
@@ -113,15 +162,22 @@ public static void main(String[] args){
     //System.out.println(series(3,0));
     //System.out.println(prime(17,2));
     //System.out.println(armstrong(372,3,0,371));
-    System.out.println(isArm(371));
+    //System.out.println(isArm(371));
+     int[]arr={2,1,3,1,4,5,1,6};
+    // ArrayList<Integer> cpy=new ArrayList<>();
+    // lastIndexOf(arr, 1, 0, cpy, 0);
+    //System.out.println(cpy.size()+" "+cpy.get(cpy.size()-1));
+    int el=1;
+    int r=0;
+    // replace(arr,1,0,0);
+    String str="aabc";
+    StringBuilder sb= new StringBuilder(str);
+    System.out.println(repadj(str,1,sb));
+    
 }
 
 
 }
-
-
-
-
 
 
 
